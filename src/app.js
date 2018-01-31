2acd9340b5453b881f5a93c347e421f362d311ae
ezavile <ezavile@gmail.com>
2018-01-31 01:22:49 -0600

    react es5 + props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 var HelloReactES5 = React.createClass({
   render: function() {
-    return React.createElement('h1', null, 'React using ES5');
+    return React.createElement('h1', null, 'React using ES5, my name is ' + this.props.name);
   }
 });
 
 ReactDOM.render(
-  React.createElement(HelloReactES5, null, null),
+  React.createElement(HelloReactES5, {name: 'Edgar'}, null),
   document.getElementById('app')
 );
