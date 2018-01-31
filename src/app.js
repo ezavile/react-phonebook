4971dd8d4daa737e23a8042200f7e5d42f1096ec
ezavile <ezavile@gmail.com>
2018-01-31 01:29:23 -0600

    react es5 nested

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
+var MyName = React.createClass({
+  render: function() {
+    return React.createElement('span', null, ' my name is ' + this.props.name);
+  }
+});
+
 var HelloReactES5 = React.createClass({
   render: function() {
-    return React.createElement('h1', null, 'React using ES5, my name is ' + this.props.name);
+    return React.createElement('h1', null, 'React using ES5', React.createElement(MyName, {name: 'Edgar'}, null));
   }
 });
 
 ReactDOM.render(
-  React.createElement(HelloReactES5, {name: 'Edgar'}, null),
+  React.createElement(HelloReactES5, null, null, null),
   document.getElementById('app')
 );
