309ad618c2f8e4ff0692c45599dce6f10e51b987
ezavile <ezavile@gmail.com>
2018-02-10 17:00:22 -0600

    enable default eslint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// var Hola = "hola"; throw warning with npm run lint
+
 class App extends Component {
   render() {
     return (
