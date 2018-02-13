9cb5b8250a4123451235f68a62b6c41ebcbe50b3
ezavile <ezavile@gmail.com>
2018-02-13 01:31:04 -0600

    add firebase

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import firebase from 'firebase';
 
 import 'reset-css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+require('dotenv').config();
+
 injectTapEventPlugin();
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+});
 
 ReactDOM.render(
   <MuiThemeProvider>
