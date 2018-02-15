eb82b7c83b0c7017c502bb607f95c462bf6db6dd
ezavile <ezavile@gmail.com>
2018-02-14 20:21:13 -0600

    add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+function getName({name='John',lastname='Doe',cel='1234567890',address='fake address'}) {
+  return name + lastname + cel + address;
+}
 
-const App = () => (
-  <p className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </p>
-);
+getName({name:'Edgar',lastname:'Zavala'})
+
+const App = () => <div className="App"><header className="App-header"><img src={logo} className="App-logo" alt="logo" /><h1 className="App-title">Welcome to React</h1></header><p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p></div>;
 
 export default App;
