84893fa5fb0dce6de4af1958ec87401fd8a97460
ezavile <ezavile@gmail.com>
2018-02-17 13:28:58 -0600

    add firebase auth

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase';
 
 import 'reset-css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const config = {
+  apiKey: 'AIzaSyAelZaz9TBwNglRr1_p1ZqgnAaz7pOyni8',
+  authDomain: 'react-phonebook-2c83a.firebaseapp.com',
+  databaseURL: 'https://react-phonebook-2c83a.firebaseio.com',
+  projectId: 'react-phonebook-2c83a',
+  storageBucket: '',
+  messagingSenderId: '993794414277',
+};
+firebase.initializeApp(config);
 
 /* eslint react/jsx-filename-extension: 0 */
 ReactDOM.render(
--- src/scenes/login/Login.jsx
+++ src/scenes/login/Login.jsx
@@ -1,20 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
+import Dialog from 'material-ui/Dialog';
+import firebase from 'firebase';
+
 import { CardImage, GoogleSvg } from '../../components';
 
 import './login.css';
 
-const Login = () => (
-  <section className="Login">
-    <CardImage
-      bg="https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=14704e761ba133f2fb71ec6a8e6e8e07&auto=format&fit=crop&w=1050&q=80"
-      title="Welcome to react-phonebook!"
-    />
-    <div className="Login-google">
-      <GoogleSvg />
-      <RaisedButton label="Login" />
-    </div>
-  </section>
-);
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      isDialogOpen: false,
+      errorMessage: '',
+    };
+  }
+
+  handleAuth = () => {
+    this.setState({ isLoading: true });
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(snap => {
+        // TODO - redirect to contacts
+        console.log(snap.user);
+      })
+      .catch(err => {
+        this.setState({
+          isLoading: false,
+          errorMessage: err.message,
+          isDialogOpen: true,
+        });
+      });
+  };
+
+  closeDialog = () => {
+    this.setState({ isDialogOpen: false });
+  };
+
+  render() {
+    const loadingComponent = (
+      <div className="u-progress">
+        <CircularProgress size={80} thickness={5} />
+      </div>
+    );
+    return (
+      <section className="Login">
+        {this.state.isLoading ? loadingComponent : null}
+        <CardImage
+          bg="https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=14704e761ba133f2fb71ec6a8e6e8e07&auto=format&fit=crop&w=1050&q=80"
+          title="Welcome to react-phonebook!"
+        />
+        <div className="Login-google">
+          <GoogleSvg />
+          <RaisedButton label="Login" onClick={this.handleAuth} />
+        </div>
+        <Dialog
+          actions={<RaisedButton label="Ok" onClick={this.closeDialog} />}
+          modal={false}
+          open={this.state.isDialogOpen}
+          onRequestClose={this.closeDialog}
+        >
+          {this.state.errorMessage}
+        </Dialog>
+      </section>
+    );
+  }
+}
 
 export default Login;
