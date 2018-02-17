9d33fb1f757e8af9fa65b1eb65cf7387769fbd06
ezavile <ezavile@gmail.com>
2018-02-17 01:31:19 -0600

    add user data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { blue900 } from 'material-ui/styles/colors';
+import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
+import FontIcon from 'material-ui/FontIcon';
+import IconButton from 'material-ui/IconButton';
+import { withRouter, Switch, Route } from 'react-router-dom';
+import * as firebase from 'firebase';
 import { Login, Contacts, Profile } from './scenes';
 
-const App = () => (
-  <main>
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route exact path="/contacts" component={Contacts} />
-      <Route exact path="/profile/:id" component={Profile} />
-    </Switch>
-  </main>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: {} };
+  }
 
-export default App;
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        this.props.history.push('/');
+      }
+      this.setState({
+        user,
+      });
+    });
+  }
+
+  handleLogout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        /* eslint react/prop-types: 0 */
+        this.props.history.push('/');
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <main>
+        {this.state.user ? (
+          <AppBar
+            title={this.state.user.displayName}
+            style={{ backgroundColor: blue900 }}
+            iconElementLeft={<Avatar src={this.state.user.photoURL} size={48} />}
+            iconElementRight={
+              <IconButton onClick={this.handleLogout}>
+                <FontIcon className="material-icons">exit_to_app</FontIcon>
+              </IconButton>
+            }
+          />
+        ) : null}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/contacts" component={Contacts} />
+          <Route exact path="/profile/:id" component={Profile} />
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default withRouter(App);
