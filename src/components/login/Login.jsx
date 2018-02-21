84b1be53a2d193d17e855bf2a4392ccb2ed1539c
ezavile <ezavile@gmail.com>
2018-02-20 19:52:40 -0600

    add login component

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import CardImage from '../card-image';
 import GoogleSvg from '../google-svg';
 
+import './login.css';
+
 const Login = () => (
-  <section>
+  <section className="Login">
     <CardImage
       image="https://images.unsplash.com/photo-1478812954026-9c750f0e89fc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=96cc9609b49dbccd31188aef0c82cfb6&auto=format&fit=crop&w=1050&q=80"
       title="react-phonebook"
     />
-    <GoogleSvg />
+    <div className="Login-google">
+      <GoogleSvg />
+      <RaisedButton label="Login" secondary={true} />
+    </div>
   </section>
 );
 
