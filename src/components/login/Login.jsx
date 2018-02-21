aff00782a9e02e7bae9897d8016a0a06de00c4dd
ezavile <ezavile@gmail.com>
2018-02-20 19:46:24 -0600

    add googlesvg icon

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardImage from '../card-image';
+import GoogleSvg from '../google-svg';
 
 const Login = () => (
   <section>
@@ -7,6 +8,7 @@ const Login = () => (
       image="https://images.unsplash.com/photo-1478812954026-9c750f0e89fc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=96cc9609b49dbccd31188aef0c82cfb6&auto=format&fit=crop&w=1050&q=80"
       title="react-phonebook"
     />
+    <GoogleSvg />
   </section>
 );
 
