5e57b0c48500048b1b606194bd5fdc453a3ffb12
ezavile <ezavile@gmail.com>
2018-02-20 19:35:02 -0600

    add material-ui

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import 'reset-css';
 import './index.css';
@@ -7,5 +8,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 /* eslint react/jsx-filename-extension: 0 */
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
